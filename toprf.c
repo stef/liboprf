dcf6c387d78a08664c6e1951a689647ebb48ec58
stf <7o5rfu92t@ctrlc.hu>
2023-09-30 17:20:46 +0200

    [mod] changed license header in .c files

--- toprf.c
+++ toprf.c
@@ -14,9 +14,9 @@
     liboprf is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the License
     along with liboprf. If not, see <http://www.gnu.org/licenses/>.
 */
 
