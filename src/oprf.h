72bb480ecf2f7932107e8783db925541683ac534
stf <7o5rfu92t@ctrlc.hu>
2025-06-24 02:12:56 +0200

    [mod] minor doc & cosmetic fixes in header files

--- src/oprf.h
+++ src/oprf.h
@@ -9,15 +9,15 @@
  * using the Ristretto255 group. It includes functions for key generation,
  * blinding inputs, evaluating OPRFs, and unblinding results.
  *
- * This implementation is based on RFC 9497: Oblivious Pseudorandom 
- * Functions (OPRFs) Using Prime-Order Groups 
- * (https://www.ietf.org/archive/id/draft-irtf-cfrg-voprf-05.html).
- * 
- * 
- * This implementation also uses hashing techniques as defined in 
+ * This implementation is based on RFC 9497: Oblivious Pseudorandom
+ * Functions (OPRFs) Using Prime-Order Groups
+ * (https://www.rfc-editor.org/rfc/rfc9497.html).
+ *
+ *
+ * This implementation also uses hashing techniques as defined in
  * RFC 9380: Hashing to Elliptic Curves
- * (https://datatracker.ietf.org/doc/draft-irtf-cfrg-hash-to-curve/).
- * 
+ * (https://www.rfc-editor.org/rfc/rfc9380).
+ *
  */
 
 #include <stdint.h>
@@ -30,7 +30,7 @@
  * @brief Generates an OPRF private key
  *
  * This is almost the `KeyGen` OPRF function defined in RFC 9497.
- * Since this library does not implement Verfiable OPRF (VOPRF) 
+ * Since this library does not implement Verfiable OPRF (VOPRF)
  * functionality, no public key is needed, so steps related to that
  * are omitted.
  *
@@ -60,62 +60,62 @@ int oprf_Finalize(const uint8_t *x, const uint16_t x_len,
  * @brief Blinds an input value for OPRF evaluation
  *
  * Implements the `Blind` OPRF function defined in RFC 9497.
- * This function converts the input into an OPRF group element and 
- * randomizes it with a scalar value `r`. Both the scalar and blinded 
+ * This function converts the input into an OPRF group element and
+ * randomizes it with a scalar value `r`. Both the scalar and blinded
  * element are returned.
  *
- * @param[in] x Input value to blind. E.g., the user's password in 
+ * @param[in] x Input value to blind. E.g., the user's password in
  *            OPAQUE (pwdU)
  * @param[in] x_len Length of the input value in bytes
  * @param[out] r Random scalar used to blind the input
- * @param[out] blinded Serialized OPRF group element, the blinded version 
+ * @param[out] alpha Serialized OPRF group element, the blinded version
  *             of `x`, used as input to `oprf_Evaluate()`
  *
  * @return 0 on success, non-zero on error
  */
 int oprf_Blind(const uint8_t *x, const uint16_t x_len,
                uint8_t r[crypto_core_ristretto255_SCALARBYTES],
-               uint8_t blinded[crypto_core_ristretto255_BYTES]);
+               uint8_t alpha[crypto_core_ristretto255_BYTES]);
 
 /**
  * @brief Evaluates a blinded input using the OPRF private key
  *
  * Implements the `Evaluate` OPRF function defined in RFC 9497.
- * This function is run by the server. It uses the server's private 
- * key `k` to evaluate the client's blinded input, producing a group 
- * element `Z` that the client can later unblind.
+ * This function is run by the server. It uses the server's private
+ * key `k` to evaluate the client's blinded input, producing a group
+ * element `beta` that the client can later unblind.
  *
  * @param[in] k OPRF private key E.g., the user's private key in OPAQUE
  *            (kU)
- * @param[in] blinded  Serialized OPRF group element, an output of 
- *            `oprf_Blind()`. For OPAQUE, this is the blinded user's 
+ * @param[in] alpha  Serialized OPRF group element, an output of
+ *            `oprf_Blind()`. For OPAQUE, this is the blinded user's
  *             password, pwdU
- * @param[out] Z Serialized OPRF group element, used as input to 
+ * @param[out] beta Serialized OPRF group element, used as input to
  *            `oprf_Unblind()`
  *
  * @return 0 on success, non-zero on error
  */
 int oprf_Evaluate(const uint8_t k[crypto_core_ristretto255_SCALARBYTES],
-                  const uint8_t blinded[crypto_core_ristretto255_BYTES],
-                  uint8_t Z[crypto_core_ristretto255_BYTES]);
+                  const uint8_t alpha[crypto_core_ristretto255_BYTES],
+                  uint8_t beta[crypto_core_ristretto255_BYTES]);
 
 /**
  * @brief Unblinds an evaluated OPRF element
  *
  * Implements the `Unblind` OPRF function defined in RFC 9497.
- * This function removes the random scalar `r` from the evaluated 
- * element `Z`, producing the unblinded output `N`.
+ * This function removes the random scalar `r` from the evaluated
+ * element `beta`, producing the unblinded output `N`.
  *
  * @param[in] r Scalar used to blind the input originally
- * @param[in] Z OPRF evaluation result from the server, an output of 
+ * @param[in] beta OPRF evaluation result from the server, an output of
  *            `oprf_Evaluate()`
- * @param[out] N Serialized OPRF group element with random scalar `r` 
+ * @param[out] N Serialized OPRF group element with random scalar `r`
  *             remove, used as input to `oprf_Finalize()`.
  *
  * @return 0 on success, non-zero on error
  */
 int oprf_Unblind(const uint8_t r[crypto_core_ristretto255_SCALARBYTES],
-                 const uint8_t Z[crypto_core_ristretto255_BYTES],
+                 const uint8_t beta[crypto_core_ristretto255_BYTES],
                  uint8_t N[crypto_core_ristretto255_BYTES]);
 
 /**
@@ -143,7 +143,7 @@ int voprf_hash_to_group(const uint8_t *msg, const uint16_t msg_len, uint8_t p[cr
  * @param[in] dst Domain separation tag (DST)
  * @param[in] dst_len The length of the DST
  * @param[in] len_in_bytes Desired number of output bytes
- * @param[out] uniform_bytes Output buffer of length `len_in_bytes` to 
+ * @param[out] uniform_bytes Output buffer of length `len_in_bytes` to
  *             receive the high entropy result
  *
  * @return 0 on success, non-zero on error
--- src/toprf.h
+++ src/toprf.h
@@ -23,12 +23,12 @@
  * @file toprf.h
  * @brief API for the Threshold Oblivious Pseudorandom Function (TOPRF)
  *        implementation
- * 
+ *
  * This file defines the structures, types, and functions for implementing
- * a Threshold Oblivious Pseudorandom Function (TOPRF) based on the 
+ * a Threshold Oblivious Pseudorandom Function (TOPRF) based on the
  * paper section 3 of the paper:
- * "TOPPSS: Cost-minimal Password-Protected Secret Sharing based on 
- * Threshold OPRF" by Stanislaw Jarecki, Aggelos Kiayias, Hugo Krawczyk, 
+ * "TOPPSS: Cost-minimal Password-Protected Secret Sharing based on
+ * Threshold OPRF" by Stanislaw Jarecki, Aggelos Kiayias, Hugo Krawczyk,
  * and Jiayu Xu, 2017 (https://eprint.iacr.org/2017/363)
  */
 
@@ -49,7 +49,7 @@ typedef struct
 #define TOPRF_Part_BYTES (crypto_core_ristretto255_BYTES + 1UL)
 
 /**
- * @brief Interpolates a polynomial of degree `t` at an arbitrary point 
+ * @brief Interpolates a polynomial of degree `t` at an arbitrary point
  *        `x: y = f(x)`
  *
  * Uses Lagrange interpolation to reconstruct the polynomial value at `x`,
@@ -65,16 +65,16 @@ void interpolate(const uint8_t x, const uint8_t t, const TOPRF_Share shares[t], 
 /**
  * @brief Computes the Lagrange coefficient for `f(x)`
  *
- * This function calculates a Lagrange coefficient for `f(x)` 
+ * This function calculates a Lagrange coefficient for `f(x)`
  * based on the index and the indices of the other contributing peers
  *
- * @param[in] index The index of the peer for which the Lagrange coefficient 
+ * @param[in] index The index of the peer for which the Lagrange coefficient
  *            is being calculated
  * @param[in] x The evaluation point for the polynomial
  * @param[in] degree Total number of shares participating (number of peers)
- * @param[in] peers Array of indices of all participating peers that 
+ * @param[in] peers Array of indices of all participating peers that
  *            contribute to the reconstruction
- * @param[out] result Output buffer to store the computed Lagrange 
+ * @param[out] result Output buffer to store the computed Lagrange
  *             coefficient
  */
 
@@ -86,28 +86,28 @@ void lcoeff(const uint8_t index, const uint8_t x, const size_t degree, const uin
  * This function calculates a lagrange coefficient for `f(0)` based on
  * the index and the indices of the other contributing peers
  *
- * @param[in] index The index of the peer for which the Lagrange coefficient 
+ * @param[in] index The index of the peer for which the Lagrange coefficient
  *            is being calculated
  * @param[in] peers_len Total number of shares in the peers
  * @param[in] peers Shares that contribute to the reconstruction
- * @param[out] result Output buffer to store the computed Lagrange 
+ * @param[out] result Output buffer to store the computed Lagrange
  *             coefficient
  */
 void coeff(const uint8_t index, const size_t peers_len, const uint8_t peers[peers_len], uint8_t result[crypto_scalarmult_ristretto255_SCALARBYTES]);
 
 /**
- * @brief Splits a secret into `n` shares using Shamir secret sharing over 
+ * @brief Splits a secret into `n` shares using Shamir secret sharing over
  *        the curve Ristretto255
  *
- * The secret is shared in a (threshold, n) scheme: any threshold number 
- * of shares can reconstruct the secret, but fewer reveal nothing. 
+ * The secret is shared in a (threshold, n) scheme: any threshold number
+ * of shares can reconstruct the secret, but fewer reveal nothing.
  * This function wraps `lcoeff()`, allowing to recover the shared
- * secret without providing `x=0` as a parameter. This is mostly for 
- * backward compatibility. 
+ * secret without providing `x=0` as a parameter. This is mostly for
+ * backward compatibility.
  *
  * @param[in] secret The scalar value to be secretly shared
  * @param[in] n The number of shares created
- * @param[in] threshold Minimum number of shares required to reconstruct 
+ * @param[in] threshold Minimum number of shares required to reconstruct
  *            the secret
  * @param[out] shares Output buffer receiving `n` generated shares
  *
@@ -119,13 +119,13 @@ void toprf_create_shares(const uint8_t secret[crypto_core_ristretto255_SCALARBYT
                          uint8_t shares[n][TOPRF_Share_BYTES]);
 
 /**
- * @brief Combines shares in the exponent using Lagrange interpolation over 
+ * @brief Combines shares in the exponent using Lagrange interpolation over
  *        the curve Ristretto255
  *
- * This function combines a threshold number of shares to recover the secret 
- * in the exponent.  It uses Lagrange interpolation over the curve 
+ * This function combines a threshold number of shares to recover the secret
+ * in the exponent. It uses Lagrange interpolation over the curve
  * Ristretto255.
- * The peers are unaware of whether they participate in threshold or 
+ * The peers are unaware of whether they participate in threshold or
  * standalone mode. Their computation remains the same in both cases.
  *
  * @param[in] response_len Number of elements in the `responses` array
@@ -139,26 +139,25 @@ int toprf_thresholdmult(const size_t response_len,
                         uint8_t result[crypto_scalarmult_ristretto255_BYTES]);
 
 /**
- * @brief Efficiently evaluates a blinded input using the private key 
+ * @brief Efficiently evaluates a blinded input using the private key
  *        in a threshold setting
- * 
  *
- * This function is the efficient threshold version of `oprf_Evaluate()` 
+ * This function is the efficient threshold version of `oprf_Evaluate()`
  * defined in oprf.h.
- * It needs to know in advance the indices of all shares that will be 
- * combined later in the `toprf_thresholdcombine()` function. This 
+ * It needs to know in advance the indices of all shares that will be
+ * combined later in the `toprf_thresholdcombine()` function. This
  * precomputation reduces the total costs and distributes them to the peers
  *
- * @param[in] k The server's secret key share. For OPAQUE, this is kU, the 
+ * @param[in] k The server's secret key share. For OPAQUE, this is kU, the
  *            user's OPRF private key
- * @param[in] blinded Serialized OPRF group element, an output of 
- *            `oprf_Blind()`. For OPAQUE, this is the blinded user's 
+ * @param[in] blinded Serialized OPRF group element, an output of
+ *            `oprf_Blind()`. For OPAQUE, this is the blinded user's
  *             password, pwdU
  * @param[in] self The index of the current peer
- * @param[in] indexes Array of indices of all peers contributing to this 
+ * @param[in] indexes Array of indices of all peers contributing to this
  *            OPRF evaluation
  * @param[in] index_len Number of participating peers (Length of `indexes`)
- * @param[out] Z Serialized OPRF group element, used as input to 
+ * @param[out] Z Serialized OPRF group element, used as input to
  *            `oprf_Unblind()`
  *
  * @return 0 on success, non-zero on error
@@ -171,7 +170,7 @@ int toprf_Evaluate(const uint8_t k[TOPRF_Share_BYTES],
 /**
  * @brief Combines the partial results to reconstruct the final OPRF output
  *
- * This function is combines the results of the toprf_Evaluate() to recover 
+ * This function is combines the results of the toprf_Evaluate() to recover
  * the shared secret in the exponent.
 
  * @param[in] response_len Number of elements in the `responses` array
@@ -196,21 +195,21 @@ typedef int (*toprf_keygencb)(void *ctx, uint8_t k[crypto_core_ristretto255_SCAL
  *
  * This function implements the 3HashTDH protocol from the paper:
  * "Threshold PAKE with Security against Compromise of All Servers"
- * (https://eprint.iacr.org/2024/1455) by Gu, Jarecki, Kedzior, 
+ * (https://eprint.iacr.org/2024/1455) by Gu, Jarecki, Kedzior,
  * Nazarian, Xu.
  *
  * Use this function to implement a threshold OPRF.
  *
  * @param[in] k A share of the secret key
- * @param[in] z A random zero-sharing of the secret key. This is a share of 
- *            a random `t`-degree polynomial that evaluates to zero, where t 
+ * @param[in] z A random zero-sharing of the secret key. This is a share of
+ *            a random `t`-degree polynomial that evaluates to zero, where t
  *            is the threshold
  * @param[in] alpha The blinded element from the client
- * @param[in] ssid_S A session-specific identifier that all participants in 
- *            the threshold evaluation must agree on (it must be the same 
+ * @param[in] ssid_S A session-specific identifier that all participants in
+ *            the threshold evaluation must agree on (it must be the same
  *            for all participants)
  * @param[in] ssid_S_len Length of the `ssid_S` identifier
- * @param[out] beta Output buffer containing the result of evaluation, to 
+ * @param[out] beta Output buffer containing the result of evaluation, to
  *             be returned to the client
  *
  * @return 0 on success, non-zero on error
